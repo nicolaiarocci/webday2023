fee9219145b5c2373d0d717be56ec46c6261cb6b
Nicola Iarocci <nicola@nicolaiarocci.com>
2023-03-10 15:23:57 +0100

    step2

---
+++ test/UnitTest1.cs
@@ -0,0 +1,29 @@
+using Microsoft.Playwright.NUnit;
+using Microsoft.Playwright;
+
+namespace test
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class Tests : PageTest
+    {
+        [Test]
+        public async Task MyTest()
+        {
+            /* Playwright.Selectors.SetTestIdAttribute("test-id"); */
+
+            await Page.GotoAsync("http://localhost:5041/");
+
+            await Page.GetByTestId("counter").ClickAsync();
+
+            await Page.GetByTestId("clickme").ClickAsync();
+
+            await Expect(Page.GetByTestId("status")).ToHaveTextAsync("Current count: 1");
+
+            await Page.GetByTestId("clickme").ClickAsync();
+
+            await Expect(Page.GetByTestId("status")).ToHaveTextAsync("Current count: 2");
+
+        }
+    }
+}
