d2f1958027be7499d94082fc5c5f5c18d2804e0e
Nicola Iarocci <nicola@nicolaiarocci.com>
2023-03-15 09:43:49 +0100

    refactor to avoid code action warnings

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,5 +1,4 @@
 using Microsoft.Playwright.NUnit;
-using Microsoft.Playwright;
 
 namespace test
 {
@@ -12,7 +11,7 @@ namespace test
         {
             /* Playwright.Selectors.SetTestIdAttribute("test-id"); */
 
-            await Page.GotoAsync("https://webday2023.nicolaiarocci.com");
+            _ = await Page.GotoAsync("https://webday2023.nicolaiarocci.com");
 
             await Page.GetByTestId("counter").ClickAsync();
 
