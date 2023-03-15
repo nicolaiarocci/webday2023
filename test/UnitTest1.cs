a0a32a95af8b82bff35dd16adc673d0a48c756f1
Nicola Iarocci <nicola@nicolaiarocci.com>
2023-03-15 09:56:37 +0100

    refactoring to get rid of suggested code actions

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,7 +1,6 @@
 using Microsoft.Playwright.NUnit;
-using Microsoft.Playwright;
 
-namespace test
+namespace Test
 {
     [Parallelizable(ParallelScope.Self)]
     [TestFixture]
@@ -12,7 +11,7 @@ namespace test
         {
             /* Playwright.Selectors.SetTestIdAttribute("test-id"); */
 
-            await Page.GotoAsync("http://localhost:5041/");
+            _ = await Page.GotoAsync("http://localhost:5041/");
 
             await Page.GetByTestId("counter").ClickAsync();
 
